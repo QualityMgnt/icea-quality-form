927fc2eba6ebb1a540af607cc9b0f7c202dfd95f
QualityMgnt <iceacc95@gmail.com>
2025-08-24 12:39:22 +0300

    Update database.js

--- database.js
+++ database.js
@@ -41,7 +41,7 @@
             box-shadow: 2px 0 5px rgba(0,0,0,0.1);
         }
         .sidebar ul li a {
-            transition: background-color 0.2s ease;
+            transition: background-color: 0.2s ease;
         }
         .sidebar ul li a:hover,
         .sidebar ul li a.active {
